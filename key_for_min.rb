50dc202365486f2c17adf5b67a0b98aa00c903fc
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-09 08:33:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil 
   else 
-    min = name_hash.min_by { |key, value| key}
+    min = name_hash.min_by { |key, value| value}
   end 
   min[0]
 end
